9481b81a54dec288696739822681860e11d9ed8f
nadia_umi <nanac351@gmail.com>
2022-05-31 23:26:30 +0800

    design login with reactjs

--- src/App.js
+++ src/App.js
@@ -3,21 +3,42 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background-img">
+      <div className="container">
+        <div className="block-content">
+          <div className="bg-content">
+            <div className='text-center'>
+              <img src={logo} className="img-fluid"  alt="logo" />
+            </div>
+            <form className='form-login'>
+              <div className="form-group">
+                <label className='d-none'>Username</label>
+                <input type="text" className="form-control" placeholder="Username"></input>
+              </div>
+              <div className="form-group">
+                <label className='d-none'>Password</label>
+                <input type="text" className="form-control" placeholder="Password"></input>
+              </div>
+              <div className='row mb-5'>
+                <div className='col-6'>
+                  <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value=""></input>
+                    <label className="form-check-label text-grey-sm">
+                      Keep me sign in
+                    </label>
+                  </div>
+                </div>
+                <div className='col-6 text-right text-grey-sm'>
+                    Forgot Password
+                </div>
+              </div>
+              <div className='text-center mb-4'>
+                <button type="submit" className="btn btn-primary mb-2">LOG IN</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'popper.js';
+
 import reportWebVitals from './reportWebVitals';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
